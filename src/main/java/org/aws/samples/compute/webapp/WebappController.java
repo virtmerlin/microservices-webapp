6e5e6897f5394ebc6f99fb24def8d3edb73a3efd
virtmerlin <merlin@sense.net>
2021-03-25 11:34:01 -0500

    Revert "Logging change"
    This reverts commit 4d2b085c31e58210fb6aaf92d74748b34a2142ee.

--- src/main/java/org/aws/samples/compute/webapp/WebappController.java
+++ src/main/java/org/aws/samples/compute/webapp/WebappController.java
@@ -46,7 +46,7 @@ public class WebappController {
                     .header("x-amzn-trace-id", getTraceHeader(segment, subsegment).toString())
                     .asString()
                     .getBody();
-            logger.info("Greeting Msg is: " + greetingMessage);
+            logger.info("Greeting is: " + greetingMessage);
         } catch (Exception e) {
             logger.error("Failed connecting Greeting API: " + e);
         }
